7eacd13b6bf230fe8f9f90fb96d35d66e5716e53
Raj <amritrjakacabu015@gmail.com>
2024-04-14 20:23:59 -0400

    Making minor config changes.

--- lua/raj/core/options.lua
+++ lua/raj/core/options.lua
@@ -1,18 +1,26 @@
-vim.cmd("let g:netrw_liststyle = 3")
 vim.g.have_nerd_font = true
 
 local opt = vim.opt
 
+-- Enable line numbers.
 opt.relativenumber = true
 opt.number = true
 
+-- Tab options.
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
+-- Search options.
 opt.ignorecase = true
 opt.smartcase = true
 
+-- Split window settings.
 opt.splitbelow = true
 opt.splitright = true
+
+-- Required for tokyodark color theme to work correctly.
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
