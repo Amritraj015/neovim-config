8a9927653d995ba0c7f65bbf7ccb613d57379dbf
Raj <amritrjakacabu015@gmail.com>
2024-04-21 19:30:19 -0400

    Formatting Lazy.lua file.

--- lua/raj/lazy.lua
+++ lua/raj/lazy.lua
@@ -1,25 +1,24 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system { 
-        'git', 
-        'clone',
-        '--filter=blob:none',
-        '--branch=stable',
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
         "https://github.com/folke/lazy.nvim.git",
-        lazypath
-    }
+        lazypath,
+    })
 end ---@diagnostic disable-next-line: undefined-field
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{ import = "raj.plugins" }, { import = "raj.plugins.lsp" }}, {
+require("lazy").setup({ { import = "raj.plugins" }, { import = "raj.plugins.lsp" } }, {
     checker = {
         enabled = true,
-        notify = false 
+        notify = false,
     },
     change_detection = {
-        notify = false
-    }
+        notify = false,
+    },
 })
